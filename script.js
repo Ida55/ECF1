b3d63a754ab597c5ddc5157c7ef51f149fb515ab
Ida <idapousse@yahoo.fr>
2020-05-29 09:38:30 +0200

    Add JS

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var usPassCombo = [
+    {
+         username: "Mario",
+         password: "1234"
+     },
+
+     {
+         username: "Luigi",
+         password: "5678" 
+     }
+
+ ]
+
+ function validate(){
+     var username = document.getElementById("username").value
+     var password = document.getElementById("password").value
+
+     for(i = 0; i < usPassCombo; i++) {
+         if(username == usPassCombo[i].username && password == usPassCombo[i].password){
+             console.log(username + "successfully logged in !!")
+         }
+     }
+ }
+
+// onsubmit="return validateForm()"
